f23d5ecae182c5ae9a8cb6364f904293b53c4044
Mark Farrugia <mark.farrugia@fiberdyne.com.au>
2018-10-08 17:02:51 +1100

    Add working loopback implementation, helper scripts
    Reqorked the original aloop implementation to have the PCM devices loop on themselves, and subscribe the the AVIRT system.

--- scripts/load.sh
+++ scripts/load.sh
@@ -4,7 +4,10 @@
 insmod avirt_core.ko
 
 # Load the additional Audio Paths
-#insmod dummy/avirt_dummyap.ko
+insmod dummy/avirt_dummyap.ko
 insmod loopback/avirt_loopbackap.ko
+
+# Run the test script
+./scripts/test_configfs.sh
 
 echo "Drivers Loaded!"
--- scripts/unload.sh
+++ scripts/unload.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+rmdir /config/avirt/streams/playback_*
+rmdir /config/avirt/streams/capture_*
+
 rm_module() {
         lsmod |grep "^$1\>" && rmmod $1 || true
 }
